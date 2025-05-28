94601324051e72bbd2e356d2bb517b9fbdba1e1d
Geretree <sommergeremia@gmail.com>
2025-05-28 14:17:12 +0200

    Game switch now Possible

--- Casino/Bank/Scripts/Bank.py
+++ Casino/Bank/Scripts/Bank.py
@@ -7,16 +7,6 @@ class ChipData:
     chip1000_chips = []
     chip5000_chips = []
 
-    chip_config_data = [
-        {"value": 5, "count": 1, "list": chip5_chips},
-        {"value": 10, "count": 50, "list": chip10_chips},
-        {"value": 50, "count": 25, "list": chip50_chips},
-        {"value": 100, "count": 25, "list": chip100_chips},
-        {"value": 500, "count": 20, "list": chip500_chips},
-        {"value": 1000, "count": 10, "list": chip1000_chips},
-        {"value": 5000, "count": 5, "list": chip5000_chips}
-    ]
-
     @staticmethod
     def get_all_chips():
         return (
@@ -31,11 +21,12 @@ class ChipData:
 
     @staticmethod
     def chip_configs():
-        return ChipData.chip_config_data
-
-    @staticmethod
-    def update_chip_count(value, new_count):
-        for config in ChipData.chip_config_data:
-            if config["value"] == value:
-                config["count"] = new_count
-                break
+        return [
+            {"value": 5, "count": 80, "list": ChipData.chip5_chips},
+            {"value": 10, "count": 50, "list": ChipData.chip10_chips},
+            {"value": 50, "count": 25, "list": ChipData.chip50_chips},
+            {"value": 100, "count": 25, "list": ChipData.chip100_chips},
+            {"value": 500, "count": 20, "list": ChipData.chip500_chips},
+            {"value": 1000, "count": 10, "list": ChipData.chip1000_chips},
+            {"value": 5000, "count": 5, "list": ChipData.chip5000_chips}
+        ]
